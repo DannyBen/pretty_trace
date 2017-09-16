f8380afae4154f753a404b3c9d7ce96fc211067d
Danny Ben Shitrit <dannyb@telesofia.com>
2017-09-16 12:53:43 +0000

    remove colsole

--- lib/pretty_trace/pretty_exception.rb
+++ lib/pretty_trace/pretty_exception.rb
@@ -12,22 +12,26 @@ module PrettyTrace
     def messages
       pretty_trace = backtrace
 
-      config.filter.each do |expression|
-        pretty_trace.reject! { |trace| trace =~ expression }
-      end
+      # config.filter.each do |expression|
+      #   pretty_trace.reject! { |trace| trace =~ expression }
+      # end
 
       pretty_trace = pretty_trace[config.range] if config.range
 
       pretty_trace.map! do |item|
-        if item =~ /(.+):(\d+):in `(.+)'(.*)/
-          file, line, method, message = $1, $2, $3, $4
-          dir = 'soon'
-          item = "line %{green}#{line.to_s.ljust 4}%{reset} in %{cyan}#{dir}%{magenta}#{file}%{reset} > %{blue}#{place}%{reset}" % colors
+        if item =~ /(.+):(\d+):in `(.+)'/
+          file, line, method = $1, $2, $3
+          dir = File.dirname(file).split('/').last
+          dir = dir == '.' ? '' : "#{dir}/"
+
+          file = File.basename file
+          item = "line %{green}#{line.to_s.ljust 4}%{reset} in %{cyan}#{dir}%{magenta}#{file}%{reset} > %{blue}#{method}%{reset}" % colors
         end
         item
       end
 
       pretty_trace
+
     end
 
     private
@@ -45,17 +49,16 @@ module PrettyTrace
     end
 
     def colors!
-      esc = 27.chr
       {
-        reset:  "#{esc}[0m",
-        black:  "#{esc}[30m",
-        red:    "#{esc}[31m",
-        green:  "#{esc}[32m",
-        yellow: "#{esc}[33m",
-        blue:   "#{esc}[34m",
-        magenta:"#{esc}[35m",
-        cyan:   "#{esc}[36m",
-        white:  "#{esc}[37m",
+        reset:  "\e[0m",
+        black:  "\e[30m",
+        red:    "\e[31m",
+        green:  "\e[32m",
+        yellow: "\e[33m",
+        blue:   "\e[34m",
+        magenta:"\e[35m",
+        cyan:   "\e[36m",
+        white:  "\e[37m",
       }
     end
   end
